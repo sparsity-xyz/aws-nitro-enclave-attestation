8d4d411c18a76ee15e9d14f4267abf5221434c41
ChenYe <git@0xdf.com>
2025-07-03 15:17:06 +0800

    add x509-verifier-rust-crypto

---
+++ crates/x509-verifier-rust-crypto/src/cert.rs
@@ -0,0 +1,196 @@
+use std::borrow::Cow;
+
+use crate::ec_decode_sig;
+use crate::verify_signature;
+use crate::KeyAlgo;
+use crate::PubKey;
+use crate::SigAlgo;
+
+use alloy_primitives::B256;
+use anyhow::anyhow;
+use anyhow::Context;
+use sha2::Sha256;
+use x509_parser::prelude::*;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Cert<'a> {
+    pub raw: X509Certificate<'a>,
+    pub bytes: &'a [u8],
+    pubkey_algo: KeyAlgo,
+}
+
+impl<'a> Cert<'a> {
+    pub fn parse_der(bytes: &'a [u8]) -> anyhow::Result<Self> {
+        let (remain, raw) = X509Certificate::from_der(bytes)
+            .map_err(|err| anyhow!("parse cert failed: {:?}", err))?;
+        if remain.len() != 0 {
+            return Err(anyhow!("parse cert not consume all bytes"));
+        }
+        let pubkey_algo = {
+            let info = raw.public_key();
+            KeyAlgo::from_algo(&info.algorithm)?
+        };
+        Ok(Self {
+            raw,
+            bytes,
+            pubkey_algo,
+        })
+    }
+
+    pub fn validity(&self) -> (ASN1Time, ASN1Time) {
+        let validity = &self.raw.validity;
+        (validity.not_before, validity.not_after)
+    }
+
+    pub fn check_valid(&self, time: ASN1Time) -> anyhow::Result<()> {
+        let validity =  &self.raw.validity;
+        if !validity.is_valid_at(time) {
+            Err(anyhow!(
+                "certificate is not valid at time: {}({}), range: {}({}) - {}({})",
+                time, time.timestamp(),
+                validity.not_before, validity.not_before.timestamp(),
+                validity.not_after, validity.not_after.timestamp(),
+            ))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn digest(&self) -> B256 {
+        sha256(self.bytes)
+    }
+
+    pub fn pubkey_algo(&self) -> anyhow::Result<KeyAlgo> {
+        let info = self.raw.public_key();
+        Ok(KeyAlgo::from_algo(&info.algorithm)?)
+    }
+
+    pub fn sig_algo(&self) -> anyhow::Result<SigAlgo> {
+        SigAlgo::from_oid(self.raw.signature_algorithm.oid())
+    }
+
+    pub fn pubkey(&self) -> PubKey {
+        PubKey {
+            algo: self.pubkey_algo.clone(),
+            val: self.raw.public_key().subject_public_key.as_ref(),
+        }
+    }
+
+    pub fn signature(&self) -> &[u8] {
+        self.raw.signature_value.as_ref()
+    }
+
+    pub fn tbs_certificate(&self) -> &[u8] {
+        self.raw.tbs_certificate.as_ref()
+    }
+
+    pub fn bytes(&self) -> &[u8] {
+        &self.bytes
+    }
+
+    pub fn verify(&self, issuer: Option<&Self>) -> anyhow::Result<bool> {
+        let issuer_key = issuer.unwrap_or(self).pubkey();
+        let sig_algo = self.sig_algo()?;
+
+        let mut sig = Cow::Borrowed(self.signature());
+        if let KeyAlgo::ECDSA(params) = issuer_key.algo {
+            sig = Cow::Owned(ec_decode_sig(&sig, params).unwrap());
+        }
+        let result = verify_signature(issuer_key, sig_algo, &sig, self.tbs_certificate()).unwrap();
+        Ok(result)
+    }
+}
+
+pub fn sha256(bytes: &[u8]) -> B256 {
+    use sha2::Digest;
+    let digest: [u8; 32] = Sha256::digest(bytes).into();
+    digest.into()
+}
+
+pub struct CertChain<'a> {
+    // cert order: root -> leaf
+    pub certs: Vec<Cert<'a>>,
+    // digest will inherit the digest of parent certs
+    pub path_digest: Vec<B256>,
+}
+
+impl<'a> CertChain<'a> {
+    pub fn new() -> Self {
+        Self {
+            certs: Vec::new(),
+            path_digest: Vec::new(),
+        }
+    }
+
+    pub fn parse<'b: 'a, I, N>(chain: I) -> anyhow::Result<Self>
+    where
+        I: IntoIterator<Item = &'b N>,
+        N: AsRef<[u8]> + 'b,
+    {
+        let mut cert_chain = Self::new();
+        for cert_der in chain {
+            cert_chain.add_cert_by_der(cert_der.as_ref())?;
+        }
+        Ok(cert_chain)
+    }
+
+    pub fn add_cert_by_der<'b: 'a>(&mut self, buf: &'b [u8]) -> anyhow::Result<()> {
+        let cert = Cert::parse_der(buf)?;
+        self.path_digest.push(match self.path_digest.last() {
+            Some(parent_digest) => sha256(&[parent_digest, &cert.digest()].concat()),
+            None => cert.digest(),
+        });
+        self.certs.push(cert);
+        Ok(())
+    }
+
+    pub fn leaf_pubkey(&self) -> PubKey {
+        self.leaf().pubkey()
+    }
+
+    pub fn leaf(&self) -> &Cert<'a> {
+        &self.certs[self.certs.len() - 1]
+    }
+
+    pub fn root(&self) -> &Cert<'a> {
+        &self.certs[0]
+    }
+
+    pub fn digest(&self) -> &[B256] {
+        &self.path_digest
+    }
+
+    pub fn check_valid(&self, timestamp: u64) -> anyhow::Result<()> {
+        let time = ASN1Time::from_timestamp(timestamp as i64)
+            .map_err(|_| anyhow!("invalid timestamp: {}", timestamp))?;
+        if self.certs.is_empty() {
+            return Err(anyhow!("cert chain is empty"));
+        }
+        for (idx, cert) in self.certs.iter().enumerate() {
+            cert.check_valid(time).with_context(|| {
+                format!("cert not valid at chain [{}/{}]", idx + 1, self.certs.len())
+            })?;
+        }
+        Ok(())
+    }
+
+    pub fn verify_chain(&self, trusted_certs_len: usize) -> anyhow::Result<bool> {
+        if trusted_certs_len > self.certs.len() {
+            return Err(anyhow!(
+                "trusted certs length is greater than cert chain length"
+            ));
+        }
+        for i in trusted_certs_len..self.certs.len() {
+            let subject = &self.certs[i];
+            let issuer = if i == 0 {
+                None
+            } else {
+                Some(&self.certs[i - 1])
+            };
+            if !subject.verify(issuer)? {
+                return Ok(false);
+            }
+        }
+        Ok(true)
+    }
+}
---
+++ crates/x509-verifier-rust-crypto/src/lib.rs
@@ -0,0 +1,138 @@
+// Override the crates by the precompiles
+
+#[cfg(feature = "sp1")]
+pub extern crate sha2_sp1 as sha2;
+#[cfg(feature = "risc0")]
+pub extern crate sha2_risc0 as sha2;
+#[cfg(all(not(feature = "sp1"), not(feature = "risc0")))]
+pub use sha2;
+
+#[cfg(feature = "sp1")]
+pub extern crate p256_sp1 as p256;
+#[cfg(feature = "risc0_unstable")]
+pub extern crate p256_risc0 as p256;
+#[cfg(all(not(feature = "sp1"), not(feature = "risc0_unstable")))]
+pub use p256;
+
+#[cfg(feature = "sp1")]
+pub extern crate rsa_sp1 as rsa;
+#[cfg(feature = "risc0_unstable")]
+pub extern crate rsa_risc0 as rsa;
+#[cfg(all(not(feature = "sp1"), not(feature = "risc0_unstable")))]
+pub use rsa;
+
+mod cert;
+pub mod constants;
+pub use cert::*;
+mod sign;
+pub use sign::*;
+
+// re-exports
+pub use x509_parser;
+
+#[cfg(test)]
+mod tests {
+    use crate::CertChain;
+    use x509_parser::prelude::*;
+
+    #[test]
+    fn test_short_sig() {
+        let certs = read_cert_chain_json("short_sig");
+        let cert_chain = CertChain::parse(&certs).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    fn read_cert_chain_json(name: &str) -> Vec<Vec<u8>> {
+        let path = format!("./samples/{}.json", name);
+        let buf = std::fs::read(path).unwrap();
+        let certs: Vec<String> = serde_json::from_slice(&buf).unwrap();
+        let certs = certs
+            .iter()
+            .map(|n| hex::decode(n.trim_start_matches("0x")).unwrap())
+            .collect::<Vec<_>>();
+        certs
+    }
+
+    #[test]
+    fn test_apple_ios_der_ecdsa() {
+        let certs = read_cert_chain_json("apple_ios_der_ec");
+        let cert_chain = CertChain::parse(&certs).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    #[test]
+    fn test_azure_snp_vek_cert() {
+        let certs = read_cert_chain_json("azure_snp_vek_cert");
+        let cert_chain = CertChain::parse(&certs).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    #[test]
+    fn test_gcp_tdx_tpm_cert() {
+        let certs = read_cert_chain_json("gcp_tdx_tpm_cert");
+        let cert_chain = CertChain::parse(&certs).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    #[test]
+    fn test_gcp_snp_tpm_cert() {
+        let certs = read_cert_chain_json("gcp_snp_tpm_cert");
+        let cert_chain = CertChain::parse(&certs).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    #[test]
+    fn test_gcp_snp_vek_cert() {
+        let certs = read_cert_chain_json("gcp_snp_vek_cert");
+        let cert_chain = CertChain::parse(&certs).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    #[test]
+    fn test_android_pem_chain() {
+        let pem_path: &str = "./samples/android-attestation.pem";
+        let pem_chain_data = std::fs::read(pem_path).expect("PEM file not found");
+        let der_chain = pem_to_der(&pem_chain_data);
+        let cert_chain = CertChain::parse(&der_chain).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    #[test]
+    fn test_vlek_ca_pem_chain() {
+        let pem_path: &str = "./samples/vlek_milan_cert_chain.pem";
+        let pem_chain_data = std::fs::read(pem_path).expect("PEM file not found");
+        let der_chain = pem_to_der(&pem_chain_data);
+        let cert_chain = CertChain::parse(&der_chain).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    #[test]
+    fn test_vcek_chain() {
+        let ca_pem_path: &str = "./samples/vcek_milan_cert_chain.pem";
+        let ca_pem_chain_data = std::fs::read(ca_pem_path).expect("PEM file not found");
+
+        let vcek_der_path: &str = "./samples/vcek.der";
+        let vcek_der = std::fs::read(vcek_der_path).expect("VCEK DER not found");
+
+        let mut der_chain = pem_to_der(&ca_pem_chain_data);
+        der_chain.insert(0, vcek_der);
+
+        let cert_chain = CertChain::parse(&der_chain).unwrap();
+        assert!(cert_chain.verify_chain().unwrap());
+    }
+
+    // Helper function
+
+    // PEM chain to DER-encoded bytes conversion
+    // Provide PEM data directly to this function call
+    fn pem_to_der(pem_chain: &[u8]) -> Vec<Vec<u8>> {
+        let mut der_chain: Vec<Vec<u8>> = Vec::new();
+
+        for pem in Pem::iter_from_buffer(pem_chain) {
+            let current_pem_content = pem.unwrap().contents;
+            der_chain.push(current_pem_content);
+        }
+
+        der_chain
+    }
+}
